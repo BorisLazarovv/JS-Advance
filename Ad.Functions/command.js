184ddfe41de5fabeab9d5a122bebbcd2b5eeaef9
BorisLazarovv <77002959+BorisLazarovv@users.noreply.github.com>
2021-03-09 08:57:05 +0200

    Add files via upload

---
+++ Ad.Functions/command.js
@@ -0,0 +1,30 @@
+function solve() {
+    let str = '';
+
+    return{
+        append,
+        removeStart,
+        removeEnd,
+        print
+    }
+
+    function append(strToAppned) {
+        str += strToAppned
+    }
+    function removeStart(n) {
+        str = str.slice(n)
+    }
+    function removeEnd(n) {
+        str = str.slice(0, -n);
+    }
+    function print() {
+        console.log(str);
+    }
+}
+let firstZeroTest = solve();
+
+firstZeroTest.append('hello');
+firstZeroTest.append('again');
+firstZeroTest.removeStart(3);
+firstZeroTest.removeEnd(4);
+firstZeroTest.print();
